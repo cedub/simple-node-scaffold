6daab423f1d2cca93f82cdb358ac97176209bb66
Kevin Mann <kevinrmann@gmail.com>
2014-04-21 09:22:27 -0600

    Removing middleware that is no longer bundled with express and needs to be installed separately

--- server/server.js
+++ server/server.js
@@ -7,22 +7,22 @@ var app = express();
 app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
+//app.use(express.logger('dev'));
+//app.use(express.json());
+//app.use(express.urlencoded());
+//app.use(express.methodOverride());
+//app.use(express.cookieParser('your secret here'));
+//app.use(express.session());
+//app.use(app.router);
 
 // set the static path to be the client folder
 var staticPath = path.dirname(path.join(__dirname))+'/client';
 app.use('/static', express.static(staticPath));
 
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+//if ('development' == app.get('env')) {
+  //app.use(express.errorHandler());
+//}
 
 // view urls
 var routes = require('./routes/routes');
